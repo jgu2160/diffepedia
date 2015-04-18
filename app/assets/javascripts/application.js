cb63983e207eceb15ff6dd356e249843b855e179
jgu <jeffwgu@gmail.com>
2015-04-18 17:47:20 -0600

    got initial venn up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap-sprockets
+//= require d3
+//= require jquery_ujs
 //= require_tree .
---
+++ app/assets/javascripts/graph.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  if(window.location.pathname === '/') {
+    var sets = [ {sets: ['A'], size: 12},
+      {sets: ['B'], size: 12},
+    {sets: ['A','B'], size: 2}];
+
+    var chart = venn.VennDiagram()
+    d3.select("#venn").datum(sets).call(chart);
+  }
+});
