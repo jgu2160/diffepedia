21ecd82576dadeec346fdde101971c8422b7116d
jgu <jeffwgu@gmail.com>
2015-04-18 18:01:07 -0600

    more initial graphs

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,10 +1,40 @@
 $(document).ready(function() {
   if(window.location.pathname === '/') {
-    var sets = [ {sets: ['A'], size: 12},
+    var simple_sets = [ {sets: ['A'], size: 12},
       {sets: ['B'], size: 12},
     {sets: ['A','B'], size: 2}];
 
+    var chart1 = venn.VennDiagram()
+
+    d3.select("#venn1").datum(simple_sets).call(chart1);
+
+    function getSetIntersections() {
+      areas = d3.selectAll(".venn_area")[0].map(
+        function (element) {
+          return { sets: element.id.split(","),
+            size: parseFloat(element.value)};} );
+            return areas;
+    }
+
+    // draw the initial set
     var chart = venn.VennDiagram()
-    d3.select("#venn").datum(sets).call(chart);
+    .width(600)
+    .height(500);
+    d3.select("#venn").datum(getSetIntersections()).call(chart);
+    // redraw the sets on any change in input
+    d3.selectAll("input").on("change", function() {
+      d3.select("#venn").datum(getSetIntersections()).call(chart);
+    });
+    // tweak style
+    var colours = ['black', 'red', 'blue'];
+    d3.selectAll("#venn .venn-circle path")
+    .style("fill-opacity", 0)
+    .style("stroke-width", 10)
+    .style("stroke-opacity", .5)
+    .style("stroke", function(d,i) { return colours[i]; });
+    d3.selectAll("#venn .venn-circle text")
+    .style("fill", function(d,i) { return colours[i]})
+    .style("font-size", "32px")
+    .style("font-weight", "100");
   }
 });
