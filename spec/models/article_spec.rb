30de4812c93bd05fb3b04536eb2f042a356ec376
jgu <jeffwgu@gmail.com>
2015-04-25 18:32:19 -0600

    added lang hash

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   let(:article) { Article.new(url: "https://en.wikipedia.org/wiki/Land_speed_record_for_rail_vehicles") }
+  let(:article_2) { Article.new(url: "https://zh-min-nan.wikipedia.org/wiki/Hûn") }
   let(:invalid_article) { Article.new(url: "https://not_valid_path.wikipedia.org/wiki/not_valid_article") }
 
   it { should have_many :user_articles}
@@ -9,6 +10,10 @@ RSpec.describe Article, type: :model do
 
   it 'validates the correct format' do
     expect(article).to be_valid
+  end
+
+  it 'validates the correct format' do
+    expect(article_2).to be_valid
   end
 
   it 'invalidates the incorrect format' do
