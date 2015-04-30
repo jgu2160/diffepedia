e99b4a5bb0f89db10fa6c5146d454c668c36593e
jgu <jeffwgu@gmail.com>
2015-04-30 15:55:55 -0600

    long articles working

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,4 +1,5 @@
 class ComparisonsController < ApplicationController
+  MAX_CHARACTERS = 10_000
   def new
   end
 
@@ -10,23 +11,18 @@ class ComparisonsController < ApplicationController
     sanitized_1 = Sanitize.fragment(lang1Text, :elements => ['p', 'h2'])
     sanitized_2 = Sanitize.fragment(lang2Text, :elements => ['p', 'h2'])
     translator = BingTranslator.new(Figaro.env.bing_id, Figaro.env.bing_secret)
-
-    begin
-      sanitized_2 = translator.translate(sanitized_2, from: lang2, to: lang1)
-    rescue
-      half = sanitized_2.length / 2
-      first_half = translator.translate(sanitized_2[0...half], from: lang2, to: lang1)
-      second_half = translator.translate(sanitized_2[half..-1], from: lang2, to: lang1)
-      sanitized_2 = first_half + second_half
+    thousands, hundreds = sanitized_2.length.divmod(MAX_CHARACTERS)
+    if thousands == 0
+      translation = translator.translate(sanitized_2, from: lang2, to: lang1)
+    else
+      translation = ""
+      0.upto(thousands - 1) do |section_index|
+        translation += translator.translate(sanitized_2[(section_index * MAX_CHARACTERS)...(section_index + 1) * MAX_CHARACTERS], from: lang2, to: lang1)
+      end
+      translation += translator.translate(sanitized_2[-hundreds..-1], from: lang2, to: lang1)
     end
 
-    hash =
-      {
-      s1: sanitized_1,
-      s2: sanitized_2,
-      l1: lang1,
-      l2: lang2
-    }
-      render json: hash
+    hash = { s1: sanitized_1, s2: translation, l1: lang1, l2: lang2 }
+    render json: hash
   end
 end
