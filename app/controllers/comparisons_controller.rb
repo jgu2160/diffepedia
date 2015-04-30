b479bb853cd638fff4db496f64b22cad052bc8b4
jgu <jeffwgu@gmail.com>
2015-04-30 16:44:49 -0600

    refactored comparison code

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -4,25 +4,8 @@ class ComparisonsController < ApplicationController
   end
 
   def create
-    lang1 = params[:comparison][:lang1]
-    lang2 = params[:comparison][:lang2]
-    lang1Text = params[:comparison][:lang1Text]
-    lang2Text = params[:comparison][:lang2Text]
-    sanitized_1 = Sanitize.fragment(lang1Text, :elements => ['p', 'h2'])
-    sanitized_2 = Sanitize.fragment(lang2Text, :elements => ['p', 'h2'])
-    translator = BingTranslator.new(Figaro.env.bing_id, Figaro.env.bing_secret)
-    thousands, hundreds = sanitized_2.length.divmod(MAX_CHARACTERS)
-    if thousands == 0
-      translation = translator.translate(sanitized_2, from: lang2, to: lang1)
-    else
-      translation = ""
-      0.upto(thousands - 1) do |section_index|
-        translation += translator.translate(sanitized_2[(section_index * MAX_CHARACTERS)...(section_index + 1) * MAX_CHARACTERS], from: lang2, to: lang1)
-      end
-      translation += translator.translate(sanitized_2[-hundreds..-1], from: lang2, to: lang1)
-    end
-
-    hash = { s1: sanitized_1, s2: translation, l1: lang1, l2: lang2 }
-    render json: hash
+    comparison = Comparison.new(params[:comparison][:lang1Text], params[:comparison][:lang2Text], params[:comparison][:lang1], params[:comparison][:lang2])
+    comparison.translate
+    render json: comparison.result
   end
 end
