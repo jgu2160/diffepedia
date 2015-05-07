000f828e741ca4117193819ebff2fd045617de97
jgu <jeffwgu@gmail.com>
2015-05-07 15:26:18 -0600

    added logout button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path
   end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback', to: "sessions#create"
   get '/session', to: "sessions#index"
+  delete "/logout", to: "sessions#destroy"
   root "comparisons#new"
   resources :comparisons
   resources :user_articles
